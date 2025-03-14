bdcb1840e4db129c88e813989de4b11ba332b440
Malcolm Paltiraja <smal0039@student.monash.edu>
2025-03-14 20:05:59 +0800

    Add health check endpoint and restructure file upload URLs (#29)

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views.file_view import FileUploadView, UserFilesView
 from .views.auth_view import PatientSignUpView, DoctorSignUpView, SignInView
+from .views.health_view import HealthView
 
 urlpatterns = [
     path('signup/patient/', PatientSignUpView.as_view(), name='signup_patient'),
     path('signup/doctor/', DoctorSignUpView.as_view(), name='signup_doctor'),
     path('signin/', SignInView.as_view(), name='signin'),
-    path('upload/', FileUploadView.as_view(), name='file-upload'),
-    path('files/', UserFilesView.as_view(), name='user-files'),
+    path('files/upload/', FileUploadView.as_view(), name='file-upload'),
+    path('files/view', UserFilesView.as_view(), name='user-files'),
+    path('health-check/', HealthView.as_view(), name='health'),
 ]
