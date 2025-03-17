0d0fd40174fbc49ac6e14c69bdd29696d0a1a6b0
Malcolm Paltiraja <smal0039@student.monash.edu>
2025-03-17 22:43:36 +0800

    Add Report models with migrations (#33)

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,5 @@
 # Import the User model here to register it with Django
 from .user import User
-
+from .report import Report
+from .hospital import Hospital
 # This file allows Django to discover your User model
