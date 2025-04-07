6c66af3722286b67a60d0f7b4a8e7bc845595dd4
smal0039 <smal0039@student.monash.edu>
2025-04-07 22:19:05 +0800

    Add AllowAny permission to HealthView

--- src/api/views/health_view.py
+++ src/api/views/health_view.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
 class HealthView(APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         return Response({"status": "ok"}, status=status.HTTP_200_OK)
